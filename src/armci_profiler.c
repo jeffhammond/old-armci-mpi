168108e438dec4142fe2d241f865dafc89e83701
Jeff Hammond <jhammond@alcf.anl.gov>
2013-08-04 13:56:49 -0500

    ordered file in non-random way

--- src/armci_profiler.c
+++ src/armci_profiler.c
@@ -1,49 +1,23 @@
-
 #include <stdio.h>
 #include "armci.h"
 #include <TAU.h>
 
 
 int
-ARMCI_AccV (int op, void *scale, armci_giov_t * darr, int len, int proc)
+ARMCI_Init ()
 {
   int rval;
-  {
-    int i, bytes = 0;
-    for (i = 0; i < len; i++)
-      bytes += darr[i].ptr_array_len * darr[i].bytes;
-    TAU_TRACE_SENDMSG (1, proc, bytes);
-  }
-  rval = PARMCI_AccV (op, scale, darr, len, proc);
+  rval = PARMCI_Init ();
   return rval;
 }
-
-
-void
-ARMCI_Barrier ()
-{
-  PARMCI_Barrier ();
-}
-
 
 int
-ARMCI_AccS (int optype, void *scale, void *src_ptr, int *src_stride_arr,
-	    void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels,
-	    int proc)
+ARMCI_Init_args (int *argc, char ***argv)
 {
   int rval;
-  {
-    int i, bytes = 1;
-    for (i = 0; i < stride_levels + 1; i++)
-      bytes *= count[i];
-    TAU_TRACE_SENDMSG (1, proc, bytes);
-  }
-  rval =
-    PARMCI_AccS (optype, scale, src_ptr, src_stride_arr, dst_ptr,
-		 dst_stride_arr, count, stride_levels, proc);
+  rval = PARMCI_Init_args (argc, argv);
   return rval;
 }
-
 
 void
 ARMCI_Finalize ()
@@ -51,56 +25,31 @@ ARMCI_Finalize ()
   PARMCI_Finalize ();
 }
 
-
-int
-ARMCI_NbPut (void *src, void *dst, int bytes, int proc,
-	     armci_hdl_t * nb_handle)
+void
+ARMCI_Fence (int proc)
 {
-  int rval;
-  TAU_TRACE_SENDMSG (1, proc, bytes);
-  rval = PARMCI_NbPut (src, dst, bytes, proc, nb_handle);
-  return rval;
+  PARMCI_Fence (proc);
 }
 
-
-int
-ARMCI_GetValueInt (void *src, int proc)
+void
+ARMCI_AllFence ()
 {
-  int rval;
-  TAU_TRACE_SENDMSG (1, proc, 4);
-  rval = PARMCI_GetValueInt (src, proc);
-  return rval;
+  PARMCI_AllFence ();
 }
 
-
-int
-ARMCI_Put_flag (void *src, void *dst, int bytes, int *f, int v, int proc)
+void
+ARMCI_Barrier ()
 {
-  int rval;
-  TAU_TRACE_SENDMSG (1, proc, bytes);
-  rval = PARMCI_Put_flag (src, dst, bytes, f, v, proc);
-  return rval;
+  PARMCI_Barrier ();
 }
 
-
 int
-ARMCI_NbGetS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-	      int *dst_stride_arr, int *count, int stride_levels, int proc,
-	      armci_hdl_t * nb_handle)
+ARMCI_Malloc (void **ptr_arr, armci_size_t bytes)
 {
   int rval;
-  {
-    int i, bytes = 1;
-    for (i = 0; i < stride_levels + 1; i++)
-      bytes *= count[i];
-    TAU_TRACE_SENDMSG (1, proc, bytes);
-  }
-  rval =
-    PARMCI_NbGetS (src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count,
-		   stride_levels, proc, nb_handle);
+  rval = PARMCI_Malloc (ptr_arr, bytes);
   return rval;
 }
-
 
 void *
 ARMCI_Malloc_local (armci_size_t bytes)
@@ -121,24 +70,67 @@ ARMCI_Free_local (void *ptr)
 
 
 int
-ARMCI_Get (void *src, void *dst, int bytes, int proc)
+ARMCI_Free (void *ptr)
 {
   int rval;
-  TAU_TRACE_SENDMSG (1, proc, bytes);
-  rval = PARMCI_Get (src, dst, bytes, proc);
+  rval = PARMCI_Free (ptr);
+  return rval;
+}
+
+void *
+ARMCI_Memat (armci_meminfo_t * meminfo, int memflg)
+{
+  void *rval;
+  rval = PARMCI_Memat (meminfo, memflg);
+  return rval;
+}
+
+void
+ARMCI_Memget (size_t bytes, armci_meminfo_t * meminfo, int memflg)
+{
+  PARMCI_Memget (bytes, meminfo, memflg);
+}
+
+int
+ARMCI_Test (armci_hdl_t * nb_handle)
+{
+  int rval;
+  rval = PARMCI_Test (nb_handle);
   return rval;
 }
 
 
 int
-ARMCI_Put (void *src, void *dst, int bytes, int proc)
+ARMCI_Wait (armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_TRACE_SENDMSG (1, proc, bytes);
-  rval = PARMCI_Put (src, dst, bytes, proc);
+  rval = PARMCI_Wait (nb_handle);
   return rval;
 }
 
+int
+ARMCI_WaitProc (int proc)
+{
+  int rval;
+  rval = PARMCI_WaitProc (proc);
+  return rval;
+}
+
+int
+ARMCI_WaitAll ()
+{
+  int rval;
+  rval = PARMCI_WaitAll ();
+  return rval;
+}
+
+int
+ARMCI_Create_mutexes (int num)
+{
+  int rval;
+  rval = PARMCI_Create_mutexes (num);
+  return rval;
+}
 
 int
 ARMCI_Destroy_mutexes ()
@@ -148,6 +140,105 @@ ARMCI_Destroy_mutexes ()
   return rval;
 }
 
+void
+ARMCI_Lock (int mutex, int proc)
+{
+  PARMCI_Lock (mutex, proc);
+}
+
+void
+ARMCI_Unlock (int mutex, int proc)
+{
+  PARMCI_Unlock (mutex, proc);
+}
+
+int
+ARMCI_Rmw (int op, int *ploc, int *prem, int extra, int proc)
+{
+  int rval;
+  TAU_TRACE_SENDMSG (1, proc, 4);
+  rval = PARMCI_Rmw (op, ploc, prem, extra, proc);
+  return rval;
+}
+
+int
+ARMCI_AccV (int op, void *scale, armci_giov_t * darr, int len, int proc)
+{
+  int rval;
+  {
+    int i, bytes = 0;
+    for (i = 0; i < len; i++)
+      bytes += darr[i].ptr_array_len * darr[i].bytes;
+    TAU_TRACE_SENDMSG (1, proc, bytes);
+  }
+  rval = PARMCI_AccV (op, scale, darr, len, proc);
+  return rval;
+}
+
+int
+ARMCI_AccS (int optype, void *scale, void *src_ptr, int *src_stride_arr,
+	    void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels,
+	    int proc)
+{
+  int rval;
+  {
+    int i, bytes = 1;
+    for (i = 0; i < stride_levels + 1; i++)
+      bytes *= count[i];
+    TAU_TRACE_SENDMSG (1, proc, bytes);
+  }
+  rval =
+    PARMCI_AccS (optype, scale, src_ptr, src_stride_arr, dst_ptr,
+		 dst_stride_arr, count, stride_levels, proc);
+  return rval;
+}
+
+int
+ARMCI_NbPut (void *src, void *dst, int bytes, int proc,
+	     armci_hdl_t * nb_handle)
+{
+  int rval;
+  TAU_TRACE_SENDMSG (1, proc, bytes);
+  rval = PARMCI_NbPut (src, dst, bytes, proc, nb_handle);
+  return rval;
+}
+
+int
+ARMCI_NbGetS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
+	      int *dst_stride_arr, int *count, int stride_levels, int proc,
+	      armci_hdl_t * nb_handle)
+{
+  int rval;
+  {
+    int i, bytes = 1;
+    for (i = 0; i < stride_levels + 1; i++)
+      bytes *= count[i];
+    TAU_TRACE_SENDMSG (1, proc, bytes);
+  }
+  rval =
+    PARMCI_NbGetS (src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count,
+		   stride_levels, proc, nb_handle);
+  return rval;
+}
+
+
+int
+ARMCI_Get (void *src, void *dst, int bytes, int proc)
+{
+  int rval;
+  TAU_TRACE_SENDMSG (1, proc, bytes);
+  rval = PARMCI_Get (src, dst, bytes, proc);
+  return rval;
+}
+
+int
+ARMCI_Put (void *src, void *dst, int bytes, int proc)
+{
+  int rval;
+  TAU_TRACE_SENDMSG (1, proc, bytes);
+  rval = PARMCI_Put (src, dst, bytes, proc);
+  return rval;
+}
 
 int
 ARMCI_GetS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
@@ -166,7 +257,6 @@ ARMCI_GetS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
   return rval;
 }
 
-
 int
 ARMCI_NbAccV (int op, void *scale, armci_giov_t * darr, int len, int proc,
 	      armci_hdl_t * nb_handle)
@@ -181,26 +271,6 @@ ARMCI_NbAccV (int op, void *scale, armci_giov_t * darr, int len, int proc,
   rval = PARMCI_NbAccV (op, scale, darr, len, proc, nb_handle);
   return rval;
 }
-
-
-float
-ARMCI_GetValueFloat (void *src, int proc)
-{
-  float rval;
-  TAU_TRACE_SENDMSG (1, proc, 4);
-  rval = PARMCI_GetValueFloat (src, proc);
-  return rval;
-}
-
-
-int
-ARMCI_Malloc (void **ptr_arr, armci_size_t bytes)
-{
-  int rval;
-  rval = PARMCI_Malloc (ptr_arr, bytes);
-  return rval;
-}
-
 
 int
 ARMCI_NbAccS (int optype, void *scale, void *src_ptr, int *src_stride_arr,
@@ -220,7 +290,6 @@ ARMCI_NbAccS (int optype, void *scale, void *src_ptr, int *src_stride_arr,
   return rval;
 }
 
-
 int
 ARMCI_PutS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
 	    int *dst_stride_arr, int *count, int stride_levels, int proc)
@@ -238,16 +307,6 @@ ARMCI_PutS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
   return rval;
 }
 
-
-void *
-ARMCI_Memat (armci_meminfo_t * meminfo, int memflg)
-{
-  void *rval;
-  rval = PARMCI_Memat (meminfo, memflg);
-  return rval;
-}
-
-
 int
 ARMCI_PutV (armci_giov_t * darr, int len, int proc)
 {
@@ -263,46 +322,7 @@ ARMCI_PutV (armci_giov_t * darr, int len, int proc)
 }
 
 
-int
-ARMCI_Free (void *ptr)
-{
-  int rval;
-  rval = PARMCI_Free (ptr);
-  return rval;
-}
 
-
-int
-ARMCI_Init_args (int *argc, char ***argv)
-{
-  int rval;
-  rval = PARMCI_Init_args (argc, argv);
-  return rval;
-}
-
-
-int
-ARMCI_PutValueInt (int src, void *dst, int proc)
-{
-  int rval;
-  TAU_TRACE_SENDMSG (1, proc, 4);
-  rval = PARMCI_PutValueInt (src, dst, proc);
-  return rval;
-}
-
-
-void
-ARMCI_Memget (size_t bytes, armci_meminfo_t * meminfo, int memflg)
-{
-  PARMCI_Memget (bytes, meminfo, memflg);
-}
-
-
-void
-ARMCI_AllFence ()
-{
-  PARMCI_AllFence ();
-}
 
 
 int
@@ -320,14 +340,6 @@ ARMCI_NbPutV (armci_giov_t * darr, int len, int proc, armci_hdl_t * nb_handle)
 }
 
 
-int
-ARMCI_PutValueDouble (double src, void *dst, int proc)
-{
-  int rval;
-  TAU_TRACE_SENDMSG (1, proc, 8);
-  rval = PARMCI_PutValueDouble (src, dst, proc);
-  return rval;
-}
 
 
 int
@@ -345,81 +357,8 @@ ARMCI_GetV (armci_giov_t * darr, int len, int proc)
 }
 
 
-int
-ARMCI_Test (armci_hdl_t * nb_handle)
-{
-  int rval;
-  rval = PARMCI_Test (nb_handle);
-  return rval;
-}
 
 
-void
-ARMCI_Unlock (int mutex, int proc)
-{
-  PARMCI_Unlock (mutex, proc);
-}
-
-
-void
-ARMCI_Fence (int proc)
-{
-  PARMCI_Fence (proc);
-}
-
-
-int
-ARMCI_Create_mutexes (int num)
-{
-  int rval;
-  rval = PARMCI_Create_mutexes (num);
-  return rval;
-}
-
-
-int
-ARMCI_PutS_flag (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-		 int *dst_stride_arr, int *count, int stride_levels,
-		 int *flag, int val, int proc)
-{
-  int rval;
-  {
-    int i, bytes = 1;
-    for (i = 0; i < stride_levels + 1; i++)
-      bytes *= count[i];
-    TAU_TRACE_SENDMSG (1, proc, bytes);
-  }
-  rval =
-    PARMCI_PutS_flag (src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count,
-		      stride_levels, flag, val, proc);
-  return rval;
-}
-
-
-int
-ARMCI_WaitProc (int proc)
-{
-  int rval;
-  rval = PARMCI_WaitProc (proc);
-  return rval;
-}
-
-
-void
-ARMCI_Lock (int mutex, int proc)
-{
-  PARMCI_Lock (mutex, proc);
-}
-
-
-double
-ARMCI_GetValueDouble (void *src, int proc)
-{
-  double rval;
-  TAU_TRACE_SENDMSG (1, proc, 8);
-  rval = PARMCI_GetValueDouble (src, proc);
-  return rval;
-}
 
 
 int
@@ -437,32 +376,8 @@ ARMCI_NbGetV (armci_giov_t * darr, int len, int proc, armci_hdl_t * nb_handle)
 }
 
 
-int
-ARMCI_Rmw (int op, int *ploc, int *prem, int extra, int proc)
-{
-  int rval;
-  TAU_TRACE_SENDMSG (1, proc, 4);
-  rval = PARMCI_Rmw (op, ploc, prem, extra, proc);
-  return rval;
-}
 
 
-int
-ARMCI_Init ()
-{
-  int rval;
-  rval = PARMCI_Init ();
-  return rval;
-}
-
-
-int
-ARMCI_WaitAll ()
-{
-  int rval;
-  rval = PARMCI_WaitAll ();
-  return rval;
-}
 
 
 int
@@ -474,17 +389,6 @@ ARMCI_NbGet (void *src, void *dst, int bytes, int proc,
   rval = PARMCI_NbGet (src, dst, bytes, proc, nb_handle);
   return rval;
 }
-
-
-int
-ARMCI_PutValueFloat (float src, void *dst, int proc)
-{
-  int rval;
-  TAU_TRACE_SENDMSG (1, proc, 4);
-  rval = PARMCI_PutValueFloat (src, dst, proc);
-  return rval;
-}
-
 
 int
 ARMCI_NbPutS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
@@ -504,6 +408,32 @@ ARMCI_NbPutS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
   return rval;
 }
 
+int
+ARMCI_Put_flag (void *src, void *dst, int bytes, int *f, int v, int proc)
+{
+  int rval;
+  TAU_TRACE_SENDMSG (1, proc, bytes);
+  rval = PARMCI_Put_flag (src, dst, bytes, f, v, proc);
+  return rval;
+}
+
+int
+ARMCI_PutS_flag (void *src_ptr, int *src_stride_arr, void *dst_ptr,
+		 int *dst_stride_arr, int *count, int stride_levels,
+		 int *flag, int val, int proc)
+{
+  int rval;
+  {
+    int i, bytes = 1;
+    for (i = 0; i < stride_levels + 1; i++)
+      bytes *= count[i];
+    TAU_TRACE_SENDMSG (1, proc, bytes);
+  }
+  rval =
+    PARMCI_PutS_flag (src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count,
+		      stride_levels, flag, val, proc);
+  return rval;
+}
 
 int
 ARMCI_PutS_flag_dir (void *src_ptr, int *src_stride_arr, void *dst_ptr,
@@ -523,6 +453,23 @@ ARMCI_PutS_flag_dir (void *src_ptr, int *src_stride_arr, void *dst_ptr,
   return rval;
 }
 
+int
+ARMCI_PutValueInt (int src, void *dst, int proc)
+{
+  int rval;
+  TAU_TRACE_SENDMSG (1, proc, 4);
+  rval = PARMCI_PutValueInt (src, dst, proc);
+  return rval;
+}
+
+int
+ARMCI_GetValueInt (void *src, int proc)
+{
+  int rval;
+  TAU_TRACE_SENDMSG (1, proc, 4);
+  rval = PARMCI_GetValueInt (src, proc);
+  return rval;
+}
 
 int
 ARMCI_PutValueLong (long src, void *dst, int proc)
@@ -533,21 +480,47 @@ ARMCI_PutValueLong (long src, void *dst, int proc)
   return rval;
 }
 
-
-int
-ARMCI_Wait (armci_hdl_t * nb_handle)
-{
-  int rval;
-  rval = PARMCI_Wait (nb_handle);
-  return rval;
-}
-
-
 long
 ARMCI_GetValueLong (void *src, int proc)
 {
   long rval;
   TAU_TRACE_SENDMSG (1, proc, 8);
   rval = PARMCI_GetValueLong (src, proc);
+  return rval;
+}
+
+int
+ARMCI_PutValueFloat (float src, void *dst, int proc)
+{
+  int rval;
+  TAU_TRACE_SENDMSG (1, proc, 4);
+  rval = PARMCI_PutValueFloat (src, dst, proc);
+  return rval;
+}
+
+float
+ARMCI_GetValueFloat (void *src, int proc)
+{
+  float rval;
+  TAU_TRACE_SENDMSG (1, proc, 4);
+  rval = PARMCI_GetValueFloat (src, proc);
+  return rval;
+}
+
+int
+ARMCI_PutValueDouble (double src, void *dst, int proc)
+{
+  int rval;
+  TAU_TRACE_SENDMSG (1, proc, 8);
+  rval = PARMCI_PutValueDouble (src, dst, proc);
+  return rval;
+}
+
+double
+ARMCI_GetValueDouble (void *src, int proc)
+{
+  double rval;
+  TAU_TRACE_SENDMSG (1, proc, 8);
+  rval = PARMCI_GetValueDouble (src, proc);
   return rval;
 }
